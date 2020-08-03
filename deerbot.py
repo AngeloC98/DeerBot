c7a0722ecaa5e37ad195b197a9f1a8b7a4cfee01
AngeloC98 <acabrasnl@gmail.com>
2020-08-03 22:13:52 +0200

    [Fix] Fixed command handler

--- deerbot.py
+++ deerbot.py
@@ -4,8 +4,6 @@ from discord.ext import commands
 
 from config import token, prefix
 
-client = discord.Client()
-
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix))
 bot.remove_command("help")
 cogs = [os.path.splitext(p)[0] for p in os.listdir("commands") if not p.startswith("_")]
@@ -14,9 +12,9 @@ for cog in cogs:
     bot.load_extension(f"commands.{cog}")
 
 
-@client.event
+@bot.event
 async def on_ready():
     print(f"DeerBot online! :)\ndiscord.py version:{discord.__version__}")
 
 
-client.run(token)
+bot.run(token)
