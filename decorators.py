a0611346f6d0d420d6d678f165bce8118ed567e5
AngeloC98 <acabrasnl@gmail.com>
2020-08-26 22:42:09 +0200

    [Fix] Added docstring for decorator

--- decorators.py
+++ decorators.py
@@ -5,6 +5,7 @@ from config import bot_owners
 
 @commands.check
 async def is_bot_owner(ctx):
+    """Checks if message author is a bot owner"""
     if ctx.author.id not in bot_owners:
         await ctx.send("Only bot owners are allowed to use this command!")
         return False
